8a5fa81f0904a2ca0eccb01da2112c3d28424355
RuanMedTI <rurato2002@gmail.com>
2020-09-27 22:08:26 -0300

    calc imc - primeiro commit

---
+++ calc.js
@@ -0,0 +1,44 @@
+const calcular = document.getElementById('calcular');
+
+
+
+function imc () {
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (altura !== '' && peso !== ''){
+        
+        const valorIMC = (peso / (altura * altura) ).toFixed(2);
+
+        let classificacao = "";
+
+        if(valorIMC < 18.5){
+            classificacao = 'Abaixo do peso!';
+        }else if(valorIMC < 25) {
+            classificacao = 'no peso ideal!';
+        }else if(valorIMC < 30){
+            classificacao = 'um pouco acima do peso!';
+        }else if(valorIMC < 35){
+            classificacao = 'obesidade grau I.';
+        }else if(valorIMC < 40){
+            classificacao = 'obesidade grau II.';
+        }else {
+            classificacao = 'obesidade grau III.';
+        }
+
+        resultado.textContent = `seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+
+    }else{
+        resultado.textContent = 'Preencha os campos restantes!';
+    }
+    
+}
+
+
+calcular.addEventListener('click', imc);
+
+
+
+
